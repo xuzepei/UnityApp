f845c18e718273a8bc9b62e09687e5bcfc2c4b3b
xuzepei <zepei.xu@smalltreemedia.com>
2019-07-23 15:34:55 +0800

    UFP demo finished.

---
+++ Assets/UFO/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		Assert.IsNotNull (player);
+
+		offset = transform.position - player.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void LateUpdate() {
+		transform.position = offset + player.transform.position;
+	}
+}
---
+++ Assets/UFO/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D rb2D;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D> ();
+
+		Assert.IsNotNull (rb2D);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2D.AddForce (movement * speed);
+	
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+}
